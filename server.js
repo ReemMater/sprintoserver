87b92c0c101f24622761af0bbbf0c3f71b79f1ca
Reem Mater <66858244+ReemMater@users.noreply.github.com>
2021-09-14 06:52:43 +0200

    j

--- server.js
+++ server.js
@@ -42,6 +42,14 @@ const corsOptions = {
 }
 
 app.use(cors(corsOptions));
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000/");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.listen(PORT, () => {
   console.log(`server start in ${PORT}`);
