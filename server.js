4e721c46ce558f123e12a53f8bc1bba88daf579f
Reem Mater <66858244+ReemMater@users.noreply.github.com>
2021-09-14 06:24:34 +0200

    finnalll

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use((req, res, next) => {
   res.setHeader('Acces-Control-Allow-Origin', '*');
+  res.setHeader("Access-Control-Allow-Credentials", true);
   res.setHeader('Acces-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
   res.setHeader('Acces-Contorl-Allow-Methods', 'Content-Type', 'Authorization');
   next();
